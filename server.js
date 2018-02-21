598c2afea5218f3841cee329e617ee6f70003fa5
Glitch (far-random) <none>
2018-02-21 09:03:04 +0000

    project finished

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 const multer = require('multer');
 const app = express();
+const upload = multer({ dest: 'uploads/' })
 
 app.use(cors());
 app.use(bodyParser.json());
@@ -10,7 +11,8 @@ app.use(bodyParser.json());
 app.use(express.static(process.cwd() + '/views'));
 
 // Routes
-app.post('/get-file-size', (req, res) => {
+app.post('/get-file-size', upload.single('file'), (req, res) => {
+  res.json({ size: req.file.size });
 });
 
 app.listen(process.env.PORT, () => {
